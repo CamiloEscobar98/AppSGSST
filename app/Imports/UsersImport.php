77cad8bc1e0b1bd48eb0f4f0100026dd6189d2b4
CamiloEscobar98 <andrescamiloye@ufps.edu.co>
2020-12-01 17:50:42 -0500

    Avances 01/12/2020

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -9,7 +9,7 @@ use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithUpserts;
 use Maatwebsite\Excel\Events\AfterSheet;
 
-class UsersImport implements ToModel, WithHeadingRow, WithEvents
+class UsersImport implements ToModel, WithHeadingRow, WithEvents, WithUpserts
 {
     /**
      * @param array $row
@@ -22,13 +22,19 @@ class UsersImport implements ToModel, WithHeadingRow, WithEvents
             'name' => $row['nombres'],
             'lastname' => $row['apellidos'],
             'email' => $row['email'],
-            'document_id' => \App\Models\Document::create([
+            'document_id' => \App\Models\Document::updateOrCreate([
                 'document' => $row['documento'],
                 'document_type_id' => \App\Models\Document_type::where('type', $row['tipodocumento'])->first()->id,
             ])->id,
             'password' => bcrypt('1234')
         ]);
     }
+
+    public function uniqueBy()
+    {
+        return 'email';
+    }
+
     public function registerEvents(): array
     {
         return [];
